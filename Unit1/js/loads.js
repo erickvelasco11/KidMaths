0d9498a3c2b6ba9583a2f1d35139a62c362b6ae9
Erick Velasco <erickvelasco11@gmail.com>
2018-04-21 12:11:53 -0500

    Add another categories for Topic1

--- Unit1/js/loads.js
+++ Unit1/js/loads.js
@@ -21,14 +21,28 @@ function filesToLoad() {
     game.load.image('titGender', 'assets/images/titles/gender.png');
     game.load.image('imgBoy', 'assets/images/boy.png');
     game.load.image('imgGirl', 'assets/images/girl.png');
-    game.load.spritesheet('btnStart', 'assets/images/buttons/btnStart.png', 200, 80);
     game.load.image('bgrTopics', 'assets/images/backgrounds/topics.png');
     game.load.image('imgOpenBox', 'assets/images/openBox.png');
+    game.load.image('itmSchoolBag', 'assets/images/items/schoolBag.png');
+    game.load.image('itmClothesbasket', 'assets/images/items/clothesbasket.png');
+    game.load.image('itmFruitBasket', 'assets/images/items/fruitBasket.png');
     game.load.image('itmApple', 'assets/images/items/apple.png');
     game.load.image('itmBananas', 'assets/images/items/bananas.png');
     game.load.image('itmStrawberry', 'assets/images/items/strawberry.png');
     game.load.image('itmPear', 'assets/images/items/pear.png');
     game.load.image('itmTomato', 'assets/images/items/tomato.png');
+    game.load.image('itmBook', 'assets/images/items/book.png');
+    game.load.image('itmErase', 'assets/images/items/erase.png');
+    game.load.image('itmPencil', 'assets/images/items/pencil.png');
+    game.load.image('itmPencilColors', 'assets/images/items/pencilColors.png');
+    game.load.image('itmPencilCase', 'assets/images/items/pencilCase.png');
+    game.load.image('itmClothCap', 'assets/images/items/clothCap.png');
+    game.load.image('itmDress', 'assets/images/items/dress.png');
+    game.load.image('itmJacket', 'assets/images/items/jacket.png');
+    game.load.image('itmShirt', 'assets/images/items/shirt.png');
+    game.load.image('itmSocks', 'assets/images/items/socks.png');
+    game.load.spritesheet('btnStart', 'assets/images/buttons/btnStart.png', 200, 80);
+    game.load.spritesheet('sprBird', 'assets/images/sprites/bird.png', 200, 200);
     //game.load.audio('battle', 'assets/sounds/battle.mp3');
 }
 
--- Unit1/js/topic1.js
+++ Unit1/js/topic1.js
@@ -1,35 +1,65 @@
 ﻿
-var fruits = ["itmApple", "itmBananas", "itmPear", "itmStrawberry", "itmTomato"]
-
+var fruits = ["itmApple", "itmBananas", "itmPear", "itmStrawberry", "itmTomato"];
+var school = ["itmBook", "itmErase", "itmPencil", "itmPencilColors", "itmPencilCase"];
+var clothes = ["itmClothCap", "itmDress", "itmJacket", "itmShirt", "itmSocks"];
+var birds;
 
 //Función para iniciar todos los componentes del tópico 1
 function initTopic1() {
     items = game.add.group();
+    birds = game.add.group();
     items.enableBody = true;
+    birds.enableBody = true;
 
-    var box1 = game.add.image(200, 500, "imgOpenBox");
+    var box1 = game.add.image(game.world.centerX/2, 500, "itmFruitBasket");
     box1.anchor.x = 0.5;
     box1.anchor.y = 0.5;
-    box1.width = 150;
-    box1.height = 150;
-    var box2 = game.add.image(600, 500, "imgOpenBox");
+    box1.width = 120;
+    box1.height = 120;
+    var box2 = game.add.image(game.world.centerX, 500, "itmSchoolBag");
     box2.anchor.x = 0.5;
     box2.anchor.y = 0.5;
-    box2.width = 150;
-    box2.height = 150;
+    box2.width = 120;
+    box2.height = 120;
+    var box3 = game.add.image(game.world.centerX + (game.world.centerX/2), 500, "itmClothesbasket");
+    box3.anchor.x = 0.5;
+    box3.anchor.y = 0.5;
+    box3.width = 120;
+    box3.height = 120;
 }
 
 function launchItemTopic1() {
-    var index = game.rnd.integerInRange(0, 4);
-    var item = items.create(-50, game.rnd.integerInRange(100, 300), fruits[index]);
+    var y = game.rnd.integerInRange(50, 300);
+    var velocity = game.rnd.integerInRange(15, 30);
+    var category = game.rnd.integerInRange(0, 2);
+    var idItem;
+    switch (category) {
+        case 0:
+            idItem = fruits[game.rnd.integerInRange(0, 4)];
+            break;
+        case 1:
+            idItem = school[game.rnd.integerInRange(0, 4)];
+            break;
+        case 2:
+            idItem = clothes[game.rnd.integerInRange(0, 4)];
+            break;
+    }
+    var item = items.create(-50, y, idItem);
     item.body.immovable = true;
     item.width = 50;
     item.height = 50;
-    item.body.velocity.x = game.rnd.integerInRange(15, 30);
+    item.body.velocity.x = velocity;
     item.body.checkCollision.up = true;
     item.body.checkCollision.down = true;
     item.body.checkCollision.left = true;
     item.body.checkCollision.right = true;
+
+    var bird = birds.create(-110, y-45, 'sprBird');
+    bird.animations.add('right', [0, 1, 2, 3, 4, 5], 15, true);
+    bird.animations.play('right');
+    bird.width = 100;
+    bird.height = 100;
+    bird.body.velocity.x = velocity;
 
     item.inputEnabled = true;
     item.input.enableDrag(false, true);
